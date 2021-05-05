7f123b0cf598a56af8061554c1ecc5f931bc9754
Matheus Homrich <matidasilva34@gmail.com>
2021-05-05 13:45:42 -0300

    scene with zoom and movement

--- DevtopiaIdleGame/DevtopiaIdleGame/MainScene.swift
+++ DevtopiaIdleGame/DevtopiaIdleGame/MainScene.swift
@@ -2,16 +2,20 @@
 //  MainScene.swift
 //  DevtopiaIdleGame
 //
-//  Created by Matheus Homrich on 03/05/21.
+//  Created by Matheus Homrich on 04/05/21.
 //
 
 import SwiftUI
+import SpriteKit
 
 struct MainScene: View {
+    
     var body: some View {
-        Image("piso")
+        SpriteView(scene: GameScene())
     }
 }
+
+
 
 struct MainScene_Previews: PreviewProvider {
     static var previews: some View {
