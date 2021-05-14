3f4563e8f135d69d1b731602cb102342f1cd1590
enzodegrazia <81195943+enzodegrazia@users.noreply.github.com>
2021-05-14 17:55:29 -0300

    Top tab bug fix

--- DevtopiaIdleGame/DevtopiaIdleGame/Scene/TopTab.swift
+++ DevtopiaIdleGame/DevtopiaIdleGame/Scene/TopTab.swift
@@ -21,41 +21,50 @@ class TopTab: SKSpriteNode {
     let coinImageRight: SKSpriteNode
     
     public init(mainCurrency: MainCurrency){
+        
+        let leftPadding: CGFloat = 5
+        dollarImage = SKSpriteNode(imageNamed: "Money")
+        dollarImage.size = CGSize(width: 20, height: 20)
+        coinImageLeft = SKSpriteNode(imageNamed: "DevCoin")
+        coinImageLeft.size = CGSize(width: 15, height: 15)
+        coinImageRight = SKSpriteNode(imageNamed: "DevCoin")
+        coinImageRight.size = CGSize(width: 15, height: 15)
+        
+        
         self.mainCurrency = mainCurrency
         //main currency per sec
         leftWhiteBar = SKSpriteNode(texture: nil, color: .white, size: CGSize(width: UIScreen.main.bounds.width * 0.25, height: UIScreen.main.bounds.height * 0.03))
         leftLabel = SKLabelNode(fontNamed: "Montserrat-Bold")
         leftLabel.text = "\(mainCurrency.decimalToString(value: mainCurrency.getDevCoinsPerSec()))/s"
         leftLabel.fontColor = .black
-        leftLabel.position = CGPoint(x: 0, y: -(leftLabel.frame.height/2) + 10)
-        leftLabel.fontSize = 15
+        coinImageLeft.position = CGPoint(x: -leftWhiteBar.size.width/2 + leftPadding + coinImageLeft.size.width/2, y: 0)
+        leftLabel.position = CGPoint(x: coinImageLeft.position.x + 10, y: 0)
+        leftLabel.fontSize = 12
+        leftLabel.horizontalAlignmentMode = .left
+        leftLabel.verticalAlignmentMode = .center
         
         //premium currency
         midWhiteBar = SKSpriteNode(texture: nil, color: .white, size: CGSize(width: UIScreen.main.bounds.width * 0.25, height: UIScreen.main.bounds.height * 0.03))
         midLabel = SKLabelNode(fontNamed: "Montserrat-Bold")
         midLabel.text = "0"
         midLabel.fontColor = .black
-        midLabel.position = CGPoint(x: 0, y: -(midLabel.frame.height/2) + 7)
-        midLabel.fontSize = 15
+        dollarImage.position = CGPoint(x: -midWhiteBar.size.width/2 + leftPadding + dollarImage.size.width/2, y: 0)
+        midLabel.position = CGPoint(x: dollarImage.position.x + 10, y: 0)
+        midLabel.fontSize = 12
+        midLabel.horizontalAlignmentMode = .left
+        midLabel.verticalAlignmentMode = .center
         
         //main currency
         rightWhiteBar = SKSpriteNode(texture: nil, color: .white, size: CGSize(width: UIScreen.main.bounds.width * 0.25, height: UIScreen.main.bounds.height * 0.03))
         rightLabel = SKLabelNode(fontNamed: "Montserrat-Bold")
         rightLabel.text = mainCurrency.decimalToString(value: mainCurrency.getDevCoins())
         rightLabel.fontColor = .black
-        rightLabel.position = CGPoint(x: 0, y: -(rightLabel.frame.height/2) + 7)
-        rightLabel.fontSize = 15
-        
-        let leftPadding: CGFloat = 5
-        dollarImage = SKSpriteNode(imageNamed: "Money")
-        dollarImage.size = CGSize(width: 20, height: 20)
-        dollarImage.position = CGPoint(x: -midWhiteBar.size.width/2 + leftPadding + dollarImage.size.width/2, y: 0)
-        coinImageLeft = SKSpriteNode(imageNamed: "DevCoin")
-        coinImageLeft.size = CGSize(width: 15, height: 15)
-        coinImageLeft.position = CGPoint(x: -leftWhiteBar.size.width/2 + leftPadding + coinImageLeft.size.width/2, y: 0)
-        coinImageRight = SKSpriteNode(imageNamed: "DevCoin")
-        coinImageRight.size = CGSize(width: 15, height: 15)
         coinImageRight.position = CGPoint(x: -rightWhiteBar.size.width/2 + leftPadding + coinImageRight.size.width/2, y: 0)
+        rightLabel.position = CGPoint(x: coinImageRight.position.x + 10, y: 0)
+        rightLabel.fontSize = 12
+        rightLabel.horizontalAlignmentMode = .left
+        rightLabel.verticalAlignmentMode = .center
+        
         
         super.init(texture: nil, color: UIColor(named: "greenTab")!, size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.1))
         self.position = CGPoint(x: 0, y: UIScreen.main.bounds.height/2 - self.frame.height/2)
