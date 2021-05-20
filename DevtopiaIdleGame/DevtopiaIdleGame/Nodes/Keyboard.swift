c807f7dc44dd33b0469001e10908a5fa243d0eea
enzodegrazia <81195943+enzodegrazia@users.noreply.github.com>
2021-05-20 16:12:42 -0300

    keyboard sounds and keyboard animation

--- DevtopiaIdleGame/DevtopiaIdleGame/Nodes/Keyboard.swift
+++ DevtopiaIdleGame/DevtopiaIdleGame/Nodes/Keyboard.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SpriteKit
+import AudioToolbox
 
 class KeyboardButton: SKSpriteNode {
     var mainCurrency: MainCurrency
@@ -29,8 +30,54 @@ class KeyboardButton: SKSpriteNode {
         let location = touch?.location(in: self)
         let node = self.atPoint(location ?? .zero)
         if node.name == "keyboardButton" {
+            
+            //            let keyboard = KeyboardButton(mainCurrency: self.mainCurrency)
+            //            let animationKeyboard = SKAction.scale(by: 0.5, duration: 1)
+            //            keyboard.run(animationKeyboard) {
+            //                keyboard.removeFromParent()
+            //            }
+            
+            
             self.mainCurrency.updateDevCoins(value: mainCurrency.getDevCoinsPerSec() * 0.2)
-            SoundController.keyboardClick(parent: self)
+            let coin = KeyboardCoin(mainCurrency: self.mainCurrency)
+            if let location = location {
+                let maxValue = self.frame.width * 0.90 / 2 - coin.frame.width / 2
+                if location.x > 0 && abs(location.x) > maxValue {
+                    coin.position = CGPoint(x: maxValue, y: 0)
+                }
+                else if location.x < 0 && abs(location.x) > maxValue{
+                    coin.position = CGPoint(x: -maxValue, y: 0)
+                }
+                else {
+                    coin.position = CGPoint(x: location.x, y: 0)
+                }
+            }
+            self.addChild(coin)
+            coin.zPosition = -1
+            let duration = 1.3
+            let moveUp = SKAction.moveBy(x: 0, y: self.frame.height, duration: duration)
+            moveUp.timingMode = .easeOut
+            let wait = SKAction.wait(forDuration: duration / 2)
+            let fadeOut = SKAction.fadeOut(withDuration: duration / 2)
+            let fadeOutSequence = SKAction.sequence([wait, fadeOut])
+            let groupAction = SKAction.group([moveUp, fadeOutSequence])
+            
+            coin.run(groupAction) {
+                coin.removeFromParent()
+            }
+            
+            let scaleDown = SKAction.scale(to: 0.95, duration: 0.05)
+            let scaleUp = SKAction.scale(to: 1, duration: 0.05)
+            let scaleSequence = SKAction.sequence([scaleDown, scaleUp])
+            let playKeyboardSound = SKAction.run {
+                            
+                SoundController.keyboardClick(parent: self)
+            }
+            let scaleGroup = SKAction.group([playKeyboardSound, scaleSequence])
+            self.run(scaleGroup)
+            
         }
     }
+    
+    
 }
